10dd4c57ffe73add9503d10b9b68dd75effeff64
tanzeelnaveed8 <tanzeelnaveed8@gmail.com>
2024-09-19 23:54:27 +0500

    project-1

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return ( 
+    <div>
+      <h1>Hello World Of Next JS</h1>
+      <p>From Tanzeel Naveed</p>
+      <p>Sir Asharib</p>
+      <p> Monday 2 to 5</p>
+    </div>
+  )
+}
